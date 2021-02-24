a0d5ee900eadb9ec35adf8ba210a7e9aa8cd1853
MikeMSN <mikeveer1@gmail.com>
2021-02-25 00:55:48 +0300

    added CustomActuatorEndpoint

---
+++ src/main/java/com/github/mosinpower/actuatorservice/CustomActuatorEndpoint.java
@@ -0,0 +1,25 @@
+package com.github.mosinpower.actuatorservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.endpoint.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id = "spec")
+public class CustomActuatorEndpoint {
+
+    @Value(value = "${mp.custom.flag:true}")
+    private String flag;
+
+    @ReadOperation
+    public String getFlag() {
+        return flag;
+    }
+
+    @WriteOperation
+    public String setNewFlag(String newFlag) {
+        flag = newFlag;
+        return flag;
+    }
+
+}
